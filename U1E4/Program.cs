db84fcd7d2458c0e7d5a82656374bba857eed208
Martina <martinaescorial9@gmail.com>
2021-06-14 16:55:18 +0200

    Agregar archivos de proyecto.

---
+++ U1E4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace U1E4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 8;
+            Console.WriteLine("N = {0}", N);
+            N += 77;
+            Console.WriteLine("N = {0}", N);
+            N -= 3;
+            Console.WriteLine("N = {0}", N);
+            N *= 2;
+            Console.WriteLine("N = {0}", N);
+        }
+    }
+}
